42c650d7d5a387d75c741c34f7bd4bc5c2135d00
yongbchen <yongbchen@ebay.com>
2014-07-26 19:46:17 +0800

    Refactor the cmsdeploy task

--- tasks/cmsdeploy.js
+++ tasks/cmsdeploy.js
@@ -7,56 +7,55 @@
  */
 
 'use strict';
-var async = require('async'),
-	clientRequest = {};
 
-function errorHandler(err){
-	// a placeHolder error handler function
-	console.log(err.message);
-}
-
-function initAutoDeploy(grunt){
-	// For the saving multiple files simultaneously
-	var changedFiles = Object.create(null);
-	var onChange = grunt.util._.debounce(function() {
-		grunt.config('cmsdeploy.app.src', Object.keys(changedFiles));
-		changedFiles = Object.create(null);
-	}, 200);
-	grunt.event.on('watch', function(action, filepath) {
-		changedFiles[filepath] = action;
-		onChange();
-	});
-}
-
-function sendRequest(settings){
-	var postData = JSON.stringify(settings.postData),
-		options = settings.remoteServer,
-		req = {};
-		
-	if(settings.isHttps){
-		options.rejectUnauthorized = false;
-	}
-	
-	req = clientRequest.request(options, function(res){
-		res.setEncoding('utf8');
-		res.on('data', function(chunk){
-			console.log('Response: ' + chunk);
-			if(typeof settings.callback === 'function'){
-				settings.callback();
-			}
-		});
-	});
-	req.write(postData);
-	req.end();
-	req.on('error', function(e){
-		console.log('Got error: ' + e.message);
-	});
-}
-	
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+	var async = require('async'),
+		clientRequest = {};
+
+	var errorHandler = function errorHandler(err){
+		// a placeHolder error handler function
+		console.log(err.message);
+	}
+
+	var initAutoDeploy = function initAutoDeploy(grunt){
+		// For the saving multiple files simultaneously
+		var changedFiles = Object.create(null);
+		var onChange = grunt.util._.debounce(function() {
+			grunt.config('cmsdeploy.app.src', Object.keys(changedFiles));
+			changedFiles = Object.create(null);
+		}, 200);
+		grunt.event.on('watch', function(action, filepath) {
+			changedFiles[filepath] = action;
+			onChange();
+		});
+	}
+
+	var sendRequest = function sendRequest(settings){
+		var postData = JSON.stringify(settings.postData),
+			options = settings.remoteServer,
+			req = {};
+			
+		if(settings.isHttps){
+			options.rejectUnauthorized = false;
+		}
+		
+		req = clientRequest.request(options, function(res){
+			res.setEncoding('utf8');
+			res.on('data', function(chunk){
+				console.log('Response: ' + chunk);
+				if(typeof settings.callback === 'function'){
+					settings.callback();
+				}
+			});
+		});
+		req.write(postData);
+		req.end();
+		req.on('error', function(e){
+			console.log('Got error: ' + e.message);
+		});
+	}
+	
 
   grunt.registerMultiTask('cmsdeploy', 'Node.js Grunt plugin. Deploy file content to a remote server', function() {
     // Merge task-specific and/or target-specific options with these defaults.
@@ -81,9 +80,13 @@ module.exports = function(grunt) {
 		files = this.filesSrc,
 		isAuto = grunt.option('auto') || false,
 		done = this.async(),
-		isHttps = options.remoteServer.protocol === 'https' ? true : false;
+		isHttps = true;
 	
+	if (options.remoteServer.protocol !== 'http' && options.remoteServer.protocol !== 'https') {
+      grunt.fatal('protocol option must be \'http\' or \'https\'');
+    }
 	
+	isHttps = options.remoteServer.protocol === 'https' ? true : false;
 	clientRequest = require(isHttps ? 'https' : 'http');
 	delete options.remoteServer.protocol;
 	grunt.log.writeln("======changedFiles=====: ", files);
